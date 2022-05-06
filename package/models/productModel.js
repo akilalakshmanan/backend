83e9c74ef123b7d3e3af0f255e20692f5dab065a
akilalakshmanan <Akila.L@iiitb.ac.in>
2022-05-06 18:16:47 +0530

    Changes for Deployment

---
+++ package/models/productModel.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = new mongoose.Schema(
+    {
+      name: { type: String, required: true },
+      comment: { type: String, required: true },
+      rating: { type: Number, required: true },
+    },
+    {
+      timestamps: true,
+    }
+);
+
+const productSchema = new mongoose.Schema(
+    {
+      productIdStripe: { type: String, required: true, unique: true},
+      name: { type: String, required: true, unique: true },
+      slug: { type: String, required: true, unique: true },
+      image: { type: String, required: true },
+      images: [String],
+      brand: { type: String, required: true },
+      category: { type: String, required: true },
+      description: { type: String, required: true },
+      price: { type: Number, required: true },
+      priceIdStripe: { type: String, required: true},
+      countInStock: { type: Number, required: true },
+      isActive: {type: Boolean, required: true}
+    },
+    {
+      timestamps: true,
+    }
+  );
+
+const Product = mongoose.model('Product',productSchema);
+export default Product;
